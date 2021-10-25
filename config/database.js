ff34deb9b701c01bc1ce151450add775ba18e206
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2021-10-25 10:53:29 -0400

    updated db

---
+++ config/database.js
@@ -0,0 +1,9 @@
+// config/database.js
+module.exports = {
+	url: 'mongodb+srv://savageAuth:pIuybFhikT6dREN7@cluster0.qbdrb.mongodb.net/roulette?retryWrites=true&w=majority',
+	dbName: 'roulette',
+};
+
+// pIuybFhikT6dREN7;
+
+//mongodb+srv://savageAuth:<password>@cluster0.qbdrb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
